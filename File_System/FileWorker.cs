033477f3565ca5a3e737985dfc1a67046c53b301
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-07-11 17:15:29 +0400

    Add project files.

---
+++ File_System/FileWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_System
+{
+    internal class FileWorker
+    {
+        public delegate void FileCreationHandler(string message);
+        public event FileCreationHandler Notify;
+        private FileStream fileWriter { get; set; }
+        private StreamReader fileReader { get; set; }
+        private string Text { get; set; }
+        private string Path { get; set; }
+        public FileWorker(string path)
+        {
+            Path = path;
+        }
+        public FileStream CreateFile()
+        {
+            fileWriter = new FileStream(Path, FileMode.Create, FileAccess.Write);
+            return fileWriter;
+        }
+        public void WriteIntoFile(string text)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            fileWriter.Write(buffer, 0, buffer.Length);
+            Notify?.Invoke($"{text} was written in the file");
+            fileWriter.Close();
+        }
+        public void ReadFromFile()
+        {
+            fileReader = new StreamReader(Path);
+            Text = fileReader.ReadToEnd();
+            Notify?.Invoke($"{Text} was read from the file");
+            fileReader.Close();
+        }
+    }
+}
---
+++ File_System/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileWorker file = new FileWorker("fileName.txt");
+            file.Notify += DisplayMessage;
+            file.CreateFile();
+            file.WriteIntoFile("Hello, Armenia!");
+            file.ReadFromFile();
+        }
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
